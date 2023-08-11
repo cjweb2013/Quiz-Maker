9b87a8d73ed51c935c6d73defd5caf8f86a612f6
cjweb2013 <wcorey@alumni.unc.edu>
2023-08-11 15:32:03 -0400

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'Home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'Home',
+    pathMatch: 'full',
+  }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
